ae12d25da3b14b48ccf14a6e0525c13b73d32eaa
Andres Osante <andres.osante@Andres-Osante-MacBook-Pro.local>
2020-12-29 11:13:33 -0600

    update darts exercise

--- javascript/darts/darts.js
+++ javascript/darts/darts.js
@@ -4,16 +4,6 @@
 //
 
 export const score = (x, y) => {
-  if (isNaN(x) || isNaN(y)) return null;
-  // distance to center is determined with pythagorean theorem
-  const distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-  if (distance > 10) return 0;
-  if (distance > 5) return 1;
-  if (distance > 1) return 5;
-  if (distance >= 0) return 10;
-};
-
-export const score2= (x, y) => {
   // distance to center is determined with pythagorean theorem
   const distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
   // favoring ternary epressions based on this article: https://medium.com/javascript-scene/nested-ternaries-are-great-361bddd0f340
