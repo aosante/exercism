89c29bb2955b5be9b60c5f3d3dde3b0383a214c4
Andres Osante <andres.osante@Andres-Osante-MacBook-Pro.local>
2020-12-28 23:12:07 -0600

    add alternate exercise to darts

--- javascript/darts/darts.js
+++ javascript/darts/darts.js
@@ -12,3 +12,14 @@ export const score = (x, y) => {
   if (distance > 1) return 5;
   if (distance >= 0) return 10;
 };
+
+export const score2= (x, y) => {
+  // distance to center is determined with pythagorean theorem
+  const distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+  // favoring ternary epressions based on this article: https://medium.com/javascript-scene/nested-ternaries-are-great-361bddd0f340
+  return (isNaN(x) || isNaN(y)) ? null :
+  distance > 10 ? 0 :
+  distance > 5 ? 1 : 
+  distance > 1 ? 5 :
+  10
+}
