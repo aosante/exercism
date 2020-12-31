ac746d2c20c3c927fa36e665896f032397c22902
Andres Osante <andres.osante@Andres-Osante-MacBook-Pro.local>
2020-12-31 14:27:45 -0600

    update armstrong numbers solution

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -2,16 +2,17 @@
 // This is only a SKELETON file for the 'Armstrong Numbers' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-// TODO, try and do this with reduce
-export const isArmstrongNumber = num => {
-  const stringNumber = num.toString();
-  const numberOfDigits = stringNumber.length;
-  let sum = 0;
-  if (num === 0 || numberOfDigits === 1) return true;
-  if (numberOfDigits === 2) return false;
-  for(const digit of stringNumber) {
-    sum += Math.pow(digit, numberOfDigits);
-  }
-  return sum === num;
-};
+// export const isArmstrongNumber = num => {
+//   const stringNumber = num.toString();
+//   const numberOfDigits = stringNumber.length;
+//   let sum = 0;
+//   if (num === 0 || numberOfDigits === 1) return true;
+//   if (numberOfDigits === 2) return false;
+//   for(const digit of stringNumber) {
+//     sum += Math.pow(digit, numberOfDigits);
+//   }
+//   return sum === num;
+// };
 
+// more concise solution using Array.prototype.reduce()
+export const isArmstrongNumber = num => [...`${num}`].reduce((sum, digit, _, array) => sum + (digit ** array.length), 0) === num;
