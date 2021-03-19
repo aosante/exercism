578fb8d6d61ef54ea39acbf3aaa77548e1eb8f13
aosante <aosantea@ucenfotec.ac.cr>
2021-03-19 12:28:35 -0600

    test change

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -2,34 +2,35 @@
 // convenience to get you started writing code faster.
 export class Clock {
   constructor(hours = 0, minutes = 0) {
-    // the first argument of setHours() is ideally, an integer between 0 and 23, representing the hour. 
-    // If a value greater than 23 is provided, the datetime will be incremented by the extra hours. (same thing happens for minutes) => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/setHours
+    // the first argument of setHours() is ideally, an integer between 0 and 23, representing the hour.
+    // If a value greater than 23 is provided, the datetime will be incremented by the extra hours.
+    // (same thing happens for minutes) => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/setHours
     this.time = new Date();
-    this.time.setHours(hours)
-    this.time.setMinutes(minutes)
+    this.time.setHours(hours);
+    this.time.setMinutes(minutes);
   }
 
   setTwoDigits(value) {
-    return value.toString().padStart(2, '0')
+    return value.toString().padStart(2, '0');
   }
 
   toString() {
-    return `${this.setTwoDigits(this.time.getHours())}:${this.setTwoDigits(this.time.getMinutes())}`
+    return `${this.setTwoDigits(this.time.getHours())}:${this.setTwoDigits(
+      this.time.getMinutes()
+    )}`;
   }
 
   plus(minutes = 0) {
-    this.time.setMinutes(minutes + this.time.getMinutes())
-    return this
+    this.time.setMinutes(minutes + this.time.getMinutes());
+    return this;
   }
 
   minus(minutes = 0) {
-      this.time.setMinutes(this.time.getMinutes() - minutes)
-      return this
+    this.time.setMinutes(this.time.getMinutes() - minutes);
+    return this;
   }
 
   equals(otherTime) {
-      return this.toString() === otherTime.toString()
+    return this.toString() === otherTime.toString();
   }
 }
-
-
