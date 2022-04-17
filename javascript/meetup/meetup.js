77ffbb0dd6bbec562cde00a968ee9c4c697206fc
aosante <aosantea@ucenfotec.ac.cr>
2022-04-16 19:43:41 -0600

    .

--- javascript/meetup/meetup.js
+++
@@ -1,8 +0,0 @@
-//
-// This is only a SKELETON file for the 'Meetup' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const meetup = () => {
-  throw new Error('Remove this statement and implement this function');
-};
