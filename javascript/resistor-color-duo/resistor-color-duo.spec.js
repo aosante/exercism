15477c8aaab2d064e2af89502d531212764daee0
aosante <aosantea@ucenfotec.ac.cr>
2022-05-13 20:43:31 -0600

    update exercise

--- javascript/resistor-color-duo/resistor-color-duo.spec.js
+++ javascript/resistor-color-duo/resistor-color-duo.spec.js
@@ -1,23 +1,23 @@
-import { decodedValue } from './resistor-color-duo.js';
+import { decodedValue } from './resistor-color-duo'
 
 describe('Resistor Colors', () => {
   test('Brown and black', () => {
-    expect(decodedValue(['brown', 'black'])).toEqual(10);
-  });
+    expect(decodedValue(['brown', 'black'])).toEqual(10)
+  })
 
   test('Blue and grey', () => {
-    expect(decodedValue(['blue', 'grey'])).toEqual(68);
-  });
+    expect(decodedValue(['blue', 'grey'])).toEqual(68)
+  })
 
   test('Yellow and violet', () => {
-    expect(decodedValue(['yellow', 'violet'])).toEqual(47);
-  });
+    expect(decodedValue(['yellow', 'violet'])).toEqual(47)
+  })
 
   test('Orange and orange', () => {
-    expect(decodedValue(['orange', 'orange'])).toEqual(33);
-  });
+    expect(decodedValue(['orange', 'orange'])).toEqual(33)
+  })
 
   test('Ignore additional colors', () => {
-    expect(decodedValue(['green', 'brown', 'orange'])).toEqual(51);
-  });
-});
+    expect(decodedValue(['green', 'brown', 'orange'])).toEqual(51)
+  })
+})
