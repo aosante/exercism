f52e1ed8e9921b61b53aba973f57a36be8c2b298
aosante <aosantea@ucenfotec.ac.cr>
2022-05-15 12:30:51 -0600

    improve triangles exercise

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -4,11 +4,16 @@
 // Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get
 
 export class Triangle {
+  #isValid
+  #uniqueSides
+
   constructor(...sides) {
     this.sides = sides
+    this.#isValid = this.#isValidTriangle()
+    this.#uniqueSides = new Set(sides).size
   }
 
-  isValidTriangle() {
+  #isValidTriangle() {
     const [side1, side2, side3] = this.sides.sort((a, b) => a - b)
     const hasPositiveSides = this.sides.every((side) => side > 0)
     const hasValidInequality = side1 + side2 >= side3
@@ -17,16 +22,16 @@ export class Triangle {
 
   get isEquilateral() {
     // since all sides are the same, the set should remove duplicates and remain with only one value
-    return this.isValidTriangle() && new Set(this.sides).size === 1
+    return this.#isValid && this.#uniqueSides === 1
   }
 
   get isIsosceles() {
     // since there are two side that are equal, the set should remove duplicates and remain with two sides or less (since equilateral triangles are also isosceles)
-    return this.isValidTriangle() && new Set(this.sides).size < 3
+    return this.#isValid && this.#uniqueSides < 3
   }
 
   get isScalene() {
     // al sizes are different and unique, so the Set's size should remain 3
-    return this.isValidTriangle() && new Set(this.sides).size === 3
+    return this.#isValid && this.#uniqueSides === 3
   }
 }
