5f761a93f70dbff5cb4f43fa1456db1f63ff6a00
Andres Osante <aosantea@ucenfotec.ac.cr>
2022-06-27 00:00:15 -0600

    scrable solution

--- javascript/scrabble-score/scrabble-score.js
+++ javascript/scrabble-score/scrabble-score.js
@@ -3,16 +3,20 @@
 // convenience to get you started writing code faster.
 //
 
-export const score = () => {
-  // 1. create an object that maps letters to values.
-  // const letterValues = {
-  //   a: 1,e: 1,i: 1,l: 1,n: 1,o: 1,r: 1,s: 1,t: 1,u: 1,
-  //   d: 2,g: 2,
-  //   b: 3,c: 3,m: 3,p: 3,
-  //   f: 4,h: 4,v: 4,w: 4,y: 4,
-  //   k: 5,
-  //   j: 8,x: 8,
-  //   q: 10,z: 10
-  // }
-  throw new Error('Remove this statement and implement this function');
+export const score = (word) => {
+  const letterValues = {
+    a: 1,e: 1,i: 1,l: 1,n: 1,o: 1,r: 1,s: 1,t: 1,u: 1,
+    d: 2,g: 2,
+    b: 3,c: 3,m: 3,p: 3,
+    f: 4,h: 4,v: 4,w: 4,y: 4,
+    k: 5,
+    j: 8,x: 8,
+    q: 10,z: 10
+  }
+
+  let scoreCount = 0
+  for (const letter of word) {
+    scoreCount = scoreCount + letterValues[letter.toLowerCase()]
+  }
+  return scoreCount
 };
