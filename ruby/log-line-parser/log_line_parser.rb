d42bff0e224a03e1f13e34e345b3b1a58b931996
Andres Osante <aosantea@ucenfotec.ac.cr>
2022-07-24 22:37:17 -0600

    nit

--- ruby/log-line-parser/log_line_parser.rb
+++ ruby/log-line-parser/log_line_parser.rb
@@ -1,15 +1,17 @@
 class LogLineParser
+  LINE_REGEX = /\[(?<log_level>[A-Z]+)\]:\s+(?<message>.+)/
+
   attr_reader :log_level,
               :message
 
   def initialize(line)
-    message_groups = /\[(?<log_level>[A-Z]+)\]:\s+(?<message>.+)/.match(line)
+    message_groups = LINE_REGEX.match(line)
 
     @message = message_groups[:message].strip
     @log_level = message_groups[:log_level].downcase
   end
 
-  def reformat(str_format = '%s (%s)')
-    str_format % [@message, @log_level] 
+  def reformat(format_string = '%s (%s)')
+    format_string % [@message, @log_level]
   end
 end
