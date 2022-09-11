b05933866e0b0755f8b1136652f970d329eba760
Andres Osante <aosantea@ucenfotec.ac.cr>
2022-09-10 19:50:03 -0600

    checkpoint

--- javascript/amusement-park/amusement-park.js
+++ javascript/amusement-park/amusement-park.js
@@ -9,9 +9,7 @@
  * @param {string} ticketId
  * @returns {Visitor} the visitor that was created
  */
-export function createVisitor(name, age, ticketId) {
-  throw new Error('Please implement the createVisitor function.');
-}
+export const createVisitor = (name, age, ticketId) => ({ name, age, ticketId })
 
 /**
  * Revokes a ticket for a visitor.
@@ -19,8 +17,10 @@ export function createVisitor(name, age, ticketId) {
  * @param {Visitor} visitor the visitor with an active ticket
  * @returns {Visitor} the visitor without a ticket
  */
-export function revokeTicket(visitor) {
-  throw new Error('Please implement the revokeTicket function.');
+export const revokeTicket = (visitor) => {
+  if (visitor.ticketId) visitor.ticketId = null
+
+  return visitor
 }
 
 /**
@@ -30,8 +30,10 @@ export function revokeTicket(visitor) {
  * @param {string} ticketId
  * @returns {string} ticket status
  */
-export function ticketStatus(tickets, ticketId) {
-  throw new Error('Please implement the ticketStatus function.');
+export const ticketStatus = (tickets, ticketId) => {
+  if (tickets[ticketId] === undefined) return 'unknown ticket id'
+  else if (tickets[ticketId] === null) return 'not sold'
+  else return `sold to ${tickets[ticketId]}`
 }
 
 /**
@@ -42,9 +44,10 @@ export function ticketStatus(tickets, ticketId) {
  * @param {string} ticketId
  * @returns {string} ticket status
  */
-export function simpleTicketStatus(tickets, ticketId) {
-  throw new Error('Please implement the simpleTicketStatus function.');
-}
+export const simpleTicketStatus = (tickets, ticketId) =>
+  tickets[ticketId] === undefined || tickets[ticketId] === null
+    ? 'invalid ticket !!!'
+    : `${tickets[ticketId]}`
 
 /**
  * Determines the version of the GTC that was signed by the visitor.
@@ -52,6 +55,6 @@ export function simpleTicketStatus(tickets, ticketId) {
  * @param {VisitorWithGtc} visitor
  * @returns {string | undefined} version
  */
-export function gtcVersion(visitor) {
-  throw new Error('Please implement the gtcVersion function.');
+export const gtcVersion = (visitor) => {
+  throw new Error('Please implement the gtcVersion function.')
 }
