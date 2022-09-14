a4be1c67b0dcebbe0f352652ffe96f1bfffe5396
Andres Osante <aosantea@ucenfotec.ac.cr>
2022-09-13 20:51:10 -0600

    lucky numbers solution

--- javascript/lucky-numbers/lucky-numbers.js
+++ javascript/lucky-numbers/lucky-numbers.js
@@ -7,9 +7,8 @@
  * @param {number[]} array2
  * @returns {number} sum of the two arrays
  */
-export function twoSum(array1, array2) {
-  throw new Error('Implement the twoSum function');
-}
+export const twoSum = (array1, array2) =>
+  Number(array1.join('')) + Number(array2.join(''))
 
 /**
  * Checks whether a number is a palindrome.
@@ -17,9 +16,8 @@ export function twoSum(array1, array2) {
  * @param {number} value
  * @returns {boolean} whether the number is a palindrome or not
  */
-export function luckyNumber(value) {
-  throw new Error('Implement the luckyNumber function');
-}
+export const luckyNumber = (value) =>
+  value.toString() === value.toString().split('').reverse().join('')
 
 /**
  * Determines the error message that should be shown to the user
@@ -28,6 +26,12 @@ export function luckyNumber(value) {
  * @param {string|null|undefined} input
  * @returns {string} error message
  */
-export function errorMessage(input) {
-  throw new Error('Implement the errorMessage function');
+export const errorMessage = (input) => {
+  let message = ''
+  const castedInput = Number(input)
+  if (!input) message = 'Required field'
+  else if (isNaN(castedInput) || castedInput === 0)
+    message = 'Must be a number besides 0'
+
+  return message
 }
