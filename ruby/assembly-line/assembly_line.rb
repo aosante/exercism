be09d4da82ec01e53db9bb23e42ba7375718ebfc
Andres Osante <aosantea@ucenfotec.ac.cr>
2022-10-03 22:08:26 -0600

    checkpoint

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -6,8 +6,27 @@ class AssemblyLine
     @speed = speed
   end
 
+  private
+
+  attr_reader :speed
+
+  def success_rate
+    case speed
+    when 1..4
+      1
+    when 5..8
+      0.9
+    when 9
+      0.8
+    when 10
+      0.77
+    end
+  end
+
+  public
+
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    speed * CAR_PER_HOUR * success_rate
   end
 
   def working_items_per_minute
