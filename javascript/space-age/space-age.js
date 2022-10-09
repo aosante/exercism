265fa1702855816f961011344ae28727968bc7d8
Andres Osante <aosantea@ucenfotec.ac.cr>
2022-10-08 21:54:09 -0600

    space-age solution

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -3,6 +3,19 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+const PLANET_YEARS = {
+  earth: 1.0,
+  jupiter: 11.862615,
+  mercury: 0.2408467,
+  mars: 1.8808158,
+  neptune: 164.79132,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  venus: 0.61519726
+}
+
+const EARTH_YEAR_IN_SECONDS = 31557600
+
+export const age = (planet, secondsOld) => parseFloat(((secondsOld / EARTH_YEAR_IN_SECONDS) / PLANET_YEARS[planet]).toFixed(2))
+
+//parseFloat(result.toFixed(2))
