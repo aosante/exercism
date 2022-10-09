61f2ea96485732cba410322a3ed30e43b5288ec3
Andres Osante <aosantea@ucenfotec.ac.cr>
2022-10-08 22:43:07 -0600

    .

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -11,11 +11,18 @@ const PLANET_YEARS = {
   neptune: 164.79132,
   saturn: 29.447498,
   uranus: 84.016846,
-  venus: 0.61519726
+  venus: 0.61519726,
 }
 
 const EARTH_YEAR_IN_SECONDS = 31557600
 
-export const age = (planet, secondsOld) => parseFloat(((secondsOld / EARTH_YEAR_IN_SECONDS) / PLANET_YEARS[planet]).toFixed(2))
-
-//parseFloat(result.toFixed(2))
+/**
+ * Calculates the age of someone living in another planet given an age in seconds
+ * @param {string} planet
+ * @param {number} secondsOld
+ * @returns {number} the age in the given planet
+ */
+export const age = (planet, secondsOld) =>
+  parseFloat(
+    (secondsOld / EARTH_YEAR_IN_SECONDS / PLANET_YEARS[planet]).toFixed(2)
+  )
