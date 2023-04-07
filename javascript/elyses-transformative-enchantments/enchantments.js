c478acb3e600c4685c0dc866df6740b56781d784
Andres Osante <aosantea@ucenfotec.ac.cr>
2023-04-07 11:55:46 -0600

    checkpoint

---
+++ javascript/elyses-transformative-enchantments/enchantments.js
@@ -0,0 +1,89 @@
+// @ts-check
+
+/**
+ * Double every card in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with every card doubled
+ */
+export const seeingDouble = (deck) => deck.map((card) => card * 2)
+
+/**
+ *  Creates triplicates of every 3 found in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with triplicate 3s
+ */
+export const threeOfEachThree = (deck) =>
+  deck.reduce((result, card) => {
+    // @ts-ignore
+    card === 3 ? result.push(3, 3, 3) : result.push(card)
+
+    return result
+  }, [])
+
+/**
+ * Extracts the middle two cards from a deck.
+ * Assumes a deck is always 10 cards.
+ *
+ * @param {number[]} deck of 10 cards
+ *
+ * @returns {number[]} deck with only two middle cards
+ */
+export const middleTwo = (deck) => deck.slice(4, 6)
+
+/**
+ * Moves the outside two cards to the middle.
+ *
+ * @param {number[]} deck with even number of cards
+ *
+ * @returns {number[]} transformed deck
+ */
+
+export const sandwichTrick = (deck) => {
+  const last = deck.pop()
+  const first = deck.shift()
+  const mid = deck.length / 2
+  // @ts-ignore
+  deck.splice(mid, 0, last, first)
+
+  return deck
+}
+
+/**
+ * Removes every card from the deck except 2s.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with only 2s
+ */
+// @ts-ignore
+export const twoIsSpecial = (deck) => {
+  throw new Error('Implement the twoIsSpecial function')
+}
+
+/**
+ * Returns a perfectly order deck from lowest to highest.
+ *
+ * @param {number[]} deck shuffled deck
+ *
+ * @returns {number[]} ordered deck
+ */
+// @ts-ignore
+export const perfectlyOrdered = (deck) => {
+  throw new Error('Implement the perfectlyOrdered function')
+}
+
+/**
+ * Reorders the deck so that the top card ends up at the bottom.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} reordered deck
+ */
+// @ts-ignore
+export const reorder = (deck) => {
+  throw new Error('Implement the reorder function')
+}
