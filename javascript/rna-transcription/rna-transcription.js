02fb888d4f1438ffc4f8e27be1d318a30599dd8b
Andres Osante <aosantea@ucenfotec.ac.cr>
2023-04-08 17:08:36 -0600

    RNA transcription exercise

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,22 @@
+//
+// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const dnaToRnaMap = {
+  G: 'C',
+  C: 'G',
+  T: 'A',
+  A: 'U',
+}
+
+/**
+ *
+ * @param {string} dnaSequence
+ * @returns {string} the dna's rna transcription sequence
+ */
+export const toRna = (dnaSequence) =>
+  dnaSequence
+    .split('')
+    .map((nucleotide) => dnaToRnaMap[nucleotide])
+    .join('')
