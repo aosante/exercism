e03faa4fb9f735896cc7a9ba33240af0e6278af2
Andres Osante <aosantea@ucenfotec.ac.cr>
2023-04-14 18:39:13 -0600

    better solution for raindrops exercise

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -3,10 +3,12 @@
 // convenience to get you started writing code faster.
 //
 
-const isMultipleOf = (number, factor) => number % factor === 0
 const SOUND_FOR_THREE = 'Pling'
 const SOUND_FOR_FIVE = 'Plang'
 const SOUND_FOR_SEVEN = 'Plong'
+
+const isMultipleOf = (number, ...factors) =>
+  factors.every((factor) => number % factor === 0)
 
 /**
  *
@@ -14,13 +16,13 @@ const SOUND_FOR_SEVEN = 'Plong'
  * @returns {string} the rain drop sound
  */
 export const convert = (number) =>
-  isMultipleOf(number, 3) && isMultipleOf(number, 5) && isMultipleOf(number, 7)
+  isMultipleOf(number, 3, 5, 7)
     ? `${SOUND_FOR_THREE}${SOUND_FOR_FIVE}${SOUND_FOR_SEVEN}`
-    : isMultipleOf(number, 3) && isMultipleOf(number, 5)
+    : isMultipleOf(number, 3, 5)
     ? `${SOUND_FOR_THREE}${SOUND_FOR_FIVE}`
-    : isMultipleOf(number, 5) && isMultipleOf(number, 7)
+    : isMultipleOf(number, 5, 7)
     ? `${SOUND_FOR_FIVE}${SOUND_FOR_SEVEN}`
-    : isMultipleOf(number, 3) && isMultipleOf(number, 7)
+    : isMultipleOf(number, 3, 7)
     ? `${SOUND_FOR_THREE}${SOUND_FOR_SEVEN}`
     : isMultipleOf(number, 3)
     ? SOUND_FOR_THREE
